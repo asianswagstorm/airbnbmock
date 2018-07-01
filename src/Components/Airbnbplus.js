2227bf81f8cd2a1c9e739fc2107431921dd111fd
asianswagstrom <brobromario@hotmail.com>
2018-07-01 17:23:07 -0400

    final

--- src/Components/Airbnbplus.js
+++ src/Components/Airbnbplus.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import '../css/Airbnbplus.css';
 
+let hrefLink = "https://www.airbnb.ca/s/select_homes?refinement_paths%5B%5D=%2Fselect_homes&allow_override%5B%5D=&s_tag=I2H15RRK";
+
 class Airbnbplus extends Component {
     constructor(){
         super();
@@ -14,11 +16,11 @@ class Airbnbplus extends Component {
         <div className="container">
           <h2 className="mb-3">Introducing Airbnb Plus</h2>
           <div className="row">
-          <section id="banner">
+            <div className="col-lg-12">
                 <div className="inner">
-                <img src="images/airbnbplus.PNG" alt=""/>
+                <a className="text tm-btn" target= "_blank" href={hrefLink}><img src="/images/airbnbplus.PNG"/></a>
                 </div>
-              </section>
+            </div>
           </div>
         </div>
         </section>  
